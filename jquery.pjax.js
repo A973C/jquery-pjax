c33b18b1c340a6a65c6e53648e57bdd84cc4d4dc
Softer <softer@lin.in.ua>
2011-12-30 03:40:39 +0200

    Set page title from fragment's  or  attribute.

--- jquery.pjax.js
+++ jquery.pjax.js
@@ -281,7 +281,11 @@
       var oldTitle = document.title,
       title = $.trim( this.find('title').remove().text() );
       if ( title ) document.title = title;
-
+      
+      // No <title>? Fragment? Look for data-title and title attributes.
+      if ( !title && options.fragment ) {
+        title = $fragment.attr('title') || $fragment.data('title')
+      }
       var state = {
         pjax: options.container,
         fragment: options.fragment,
